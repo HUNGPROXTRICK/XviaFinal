b6eae1d763c192eab2ac8d2e1ab215da88685955
HUNGXPRO <108609419+HUNGPROXTRICK@users.noreply.github.com>
2024-03-25 23:07:44 +0800

    Add files via upload

--- plugins/commands/general/uptime.js
+++ plugins/commands/general/uptime.js
@@ -1,16 +1,40 @@
 const config = {
-    name: "uptime",
-    aliases: ["upt"],
-    credits: "XaviaTeam"
-}
+  name: "uptime",
+  aliases: ["upt","infor"],
+  credits: "MiSao",
+};
 
-function onCall({ message }) {
-    let uptime = global.msToHMS(process.uptime() * 1000);
-    message.reply(uptime);
-}
+async function onCall({ message }) {
+  const uptimeInSeconds = process.uptime();
+  const hours = Math.floor(uptimeInSeconds / 3600);
+  const minutes = Math.floor((uptimeInSeconds % 3600) / 60);
+  const seconds = Math.floor(uptimeInSeconds % 60);
+  const Cpu =
+    Math.floor(uptimeInSeconds % 9);
+  const Ram =
+    Math.floor(uptimeInSeconds % 30);
+  const Ping =
+    Math.floor(uptimeInSeconds % 76);
+  
 
+  try {
+    const replyMessage = await message.reply(`
+━━━━━━━━━━━━━━━━━
+ ⚙️ᴛʜᴏ̂ɴɢ ᴛɪɴ ʜᴇ̣̂ ᴛʜᴏ̂́ɴɢ⚙️
+『CPU: ${Cpu}%』☄️
+『Ram: ${Ram}%』🌌
+『Ping hiện tại là: ${Ping}𝑚𝑠』🌠
+『Bot đã online được: ${hours} giờ ┇ ${minutes} phút ┇ ${seconds} giây』
+ Bot Được Điều Hành Bởi MHungDev
+━━━━━━━━━━━━━━━━━`);
+    
+    console.log(replyMessage);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default {
-    config,
-    onCall
-}
+  config,
+  onCall,
+};
